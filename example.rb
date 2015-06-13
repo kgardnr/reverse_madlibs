97eb12e9847caf11041ea71631d00037e8b27e40
Kim Gardner <kgardnr@gmail.com>
2015-06-13 19:11:50 -0400

    First version of madlibs

--- example.rb
+++ example.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
+FILE = ARGV[0]
+
+dictionary = {
+  nouns: %w(dog car boy table), 
+  verbs: %w(run skip jump laugh),
+  adjectives: %w(big small great funny orange)
+}
+
 def say(message)
   puts "=> #{message}"
 end
@@ -8,4 +18,14 @@ def exit_with(message)
 end
 
 exit_with("Error: No input file.") if ARGV.empty?
-exit_with("Error: No such file exists.") if !File.exists?(ARGV[0])
+exit_with("Error: No such file exists.") if !File.exists?(FILE)
+
+contents = File.open(FILE, "r") do |f|
+  f.read
+end
+
+contents.gsub!("NOUN", dictionary[:nouns].sample)
+contents.gsub!("VERB", dictionary[:verbs].sample)
+contents.gsub!("ADJECTIVE", dictionary[:adjectives].sample)
+
+p contents
