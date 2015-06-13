5c8928ad5169effa1ade7e5cfbb2b6c0c5127bf5
Kim Gardner <kgardnr@gmail.com>
2015-06-13 18:38:33 -0400

    Add parens for method calls

--- example.rb
+++ example.rb
@@ -2,5 +2,5 @@ def say(message)
   puts "=> #{message}"
 end
 
-say "Error: No input file." && exit if ARGV.empty?
-say "Error: No such file exists." && exit if !File.exists?(ARGV[0])
+say("Error: No input file.") && exit if ARGV.empty?
+say("Error: No such file exists.") && exit if !File.exists?(ARGV[0])
